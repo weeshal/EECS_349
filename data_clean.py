0fc6b48c08058877ed09f2cd099af51de6629e54
Vishal Giridhar <vishalgiridhar2020@u.northwestern.edu>
2018-06-11 13:52:57 -0500

    newest dataset

--- data_clean.py
+++ data_clean.py
@@ -5,68 +5,86 @@ from collections import Counter
 
 
 
-df = pd.read_csv("music_clean.csv")
+df = pd.read_csv("newer_music.csv")
 genre = df['terms']
-
-for i,v in genre.items():
-    if "rock" in str(v):
-        genre[i] = "rock"
-    if "metal" in str(v):
-        genre[i] = "metal"
-    if "blues" in str(v):
-        genre[i] = "blues"
-    if "pop" in str(v):
-        genre[i] = "pop"
-    if "jazz" in str(v):
-        genre[i] = "jazz"
-    if "alternative" in str(v):
-        genre[i] = "alternative"
-    if "soul" in str(v):
-        genre[i] = "soul"
-    if "gospel" in str(v):
-        genre[i] = "gospel"
-    if "indie" in str(v):
-        genre[i] = "indie"
-    if "country" in str(v):
-        genre[i] = "country"
-    if "core" in str(v):
-        genre[i] = "metal"
-    if "garage" in str(v):
-        genre[i] = "garage"
-    if "filk" in str(v):
-        genre[i] = "folk"
-    if "folk" in str(v):
-        genre[i] = "folk"
-    if "grunge" in str(v):
-        genre[i] = "grunge"
-    if "swing" in str(v):
-        genre[i] = "swing"
-    if ("reggae" in str(v)) and (str(v) != "reggaeton"):
-        genre[i] = "reggae"
-    if "rap" in str(v):
-        genre[i] = "rap"
-    if "hop" in str(v):
-        genre[i] = "hip-hop"
-    if "punk" in str(v):
-        genre[i] = "punk"
-    if "house" in str(v):
-        genre[i] = "house"
-    if "vocalist" in str(v):
-        genre[i] = "vocalist"
-    if "zydeco" in str(v):
-        genre[i] = "blues"
-    if "trance" in str(v):
-        genre[i] = "trance"
-
-print(genre)
+#
+# for i,v in genre.items():
+#     if "rock" in str(v):
+#         genre[i] = "rock"
+#     if "metal" in str(v):
+#         genre[i] = "metal"
+#     if "blues" in str(v):
+#         genre[i] = "blues"
+#     if "pop" in str(v):
+#         genre[i] = "pop"
+#     if "jazz" in str(v):
+#         genre[i] = "jazz"
+#     if "alternative" in str(v):
+#         genre[i] = "alternative"
+#     if "soul" in str(v):
+#         genre[i] = "soul"
+#     if "gospel" in str(v):
+#         genre[i] = "gospel"
+#     if "indie" in str(v):
+#         genre[i] = "indie"
+#     if "country" in str(v):
+#         genre[i] = "country"
+#     if "core" in str(v):
+#         genre[i] = "metal"
+#     if "garage" in str(v):
+#         genre[i] = "garage"
+#     if "filk" in str(v):
+#         genre[i] = "folk"
+#     if "folk" in str(v):
+#         genre[i] = "folk"
+#     if "grunge" in str(v):
+#         genre[i] = "grunge"
+#     if "swing" in str(v):
+#         genre[i] = "swing"
+#     if ("reggae" in str(v)) and (str(v) != "reggaeton"):
+#         genre[i] = "reggae"
+#     if "rap" in str(v):
+#         genre[i] = "rap"
+#     if "hop" in str(v):
+#         genre[i] = "hip-hop"
+#     if "punk" in str(v):
+#         genre[i] = "punk"
+#     if "house" in str(v):
+#         genre[i] = "house"
+#     if "vocalist" in str(v):
+#         genre[i] = "vocalist"
+#     if "zydeco" in str(v):
+#         genre[i] = "blues"
+#     if "trance" in str(v):
+#         genre[i] = "trance"
+#     if "dance" in str(v):
+#         genre[i] = "dance"
+#     if "dub" in str(v):
+#         genre[i] = "dance"
+#
+# print(genre)
 
 sum = 0
 count = Counter(df['terms'])
+bad_genres = []
 
-for i in count.values():
+for i in range(len(count.values())):
     sum = sum + 1
-print(sum)
-print(count)
-df['terms'] = genre
+    if count.values()[i] < 40:
+        bad_genres.append(count.keys()[i])
 
-df.to_csv("new_music.csv")
+print(sum)
+print(bad_genres)
+
+for b in bad_genres:
+    df = df[df.terms != b]
+
+df.reset_index(drop=True)
+print(df)
+# for i in df.rows():
+#     print(i)
+#     print(i['terms'])
+    # if i['terms'] in bad_genres:
+    #     df.drop(i)
+#
+# df.to_csv("newest_music.csv")
